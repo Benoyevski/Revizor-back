4f2d309e3f1f933e82919e765f83082ef99e7fde
”Ismail” <ibeno95@mail.ru>
2022-09-28 11:45:43 +0300

    Накидал скелет

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/public",express.static(path.resolve(__dirname, "public")));
+
+
+//здесь будут роуты
+
+
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => console.log("mongoose connect"))
+  .catch(() => console.log("warning")); 
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server: ${process.env.SERVER_PORT} had been started`);
+});
