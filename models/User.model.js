a1b8746df35d7cdacec9037955cd6fdbf2d96f08
Ayub <aub9@mail.ru>
2022-10-03 15:11:02 +0300

    create back

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   login: String,
   password: String,
-  email: String,
+  mail: String,
   comment: {
     type: mongoose.SchemaTypes.ObjectId,
     ref: "Comment",
