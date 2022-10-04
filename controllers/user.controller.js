6e5d82cbd61d205aa74f0033f2e678f2082dcf70
GooA-NA <nasirgudesev@gmail.com>
2022-10-04 20:35:49 +0300

    Бэк для фото

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const fs = require("fs");
+const fs = require('fs')
 
 module.exports.user = {
   getUsers: async (req, res) => {
@@ -10,35 +10,31 @@ module.exports.user = {
   },
 
   login: async (req, res) => {
-    try {
-      const { login, password } = req.body;
-      const candidate = await User.findOne({ login });
-      if (!candidate) {
-        return res.status(401).json("User not find");
-      }
-
-      const valid = await bcrypt.compare(password, candidate.password);
-      if (!valid) {
-        return res.status(401).json("password wrong");
-      }
-      const payload = {
-        id: candidate._id,
-      };
-      const token = await jwt.sign(payload, process.env.SECRET_JWT, {
-        expiresIn: "95h",
-      });
-      res.json({
-        token,
-        id: candidate._id,
-      });
-    } catch (e) {
-      res.json(error);
+    const { login, password } = req.body;
+    const candidate = await User.findOne({ login });
+    if (!candidate) {
+      return res.status(401).json("User not find");
     }
+
+    const valid = await bcrypt.compare(password, candidate.password);
+    if (!valid) {
+      return res.status(401).json("password wrong");
+    }
+    const payload = {
+      id: candidate._id,
+    };
+    const token = await jwt.sign(payload, process.env.SECRET_JWT, {
+      expiresIn: "95h",
+    });
+    res.json({
+      token,
+      id: candidate._id,
+    });
   },
 
   registr: async (req, res) => {
     try {
-      const { login, password, mail } = req.body;
+      const { login, password } = req.body;
       const hash = await bcrypt.hash(
         password,
         Number(process.env.BCRYPT_ROUNDS)
@@ -54,7 +50,6 @@ module.exports.user = {
       res.json({ error: e });
     }
   },
-
   addAvatar: async (req, res) => {
     try {
       const file = req.files.file;
