aaf814cce91005dcb38a1a8235dd8c3af9bc253c
”Ismail” <ibeno95@mail.ru>
2022-10-06 11:45:09 +0300

    бэк для чата

--- index.js
+++ index.js
@@ -4,14 +4,46 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
-const fileUpload = require('express-fileupload')
-
-app.use(fileUpload({}))
+const fileUpload = require("express-fileupload");
+const http = require("http");
+const { Server } = require("socket.io");
+app.use(fileUpload({}));
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static(path.resolve(__dirname, "public")));
+
+//chat
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Чат сервер запущен");
+});
 
 //здесь будут роуты
 app.use(require("./routes/diners.route"));
