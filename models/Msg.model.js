798721a629fa1e57477d9a0fc0cb0bc438b5d7c5
”Ismail” <ibeno95@mail.ru>
2022-10-07 20:15:17 +0300

    добавил время в модель сообщения

--- models/Msg.model.js
+++ models/Msg.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const msgSchema = mongoose.Schema({
-  text: String,
-  author: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "User",
+const msgSchema = mongoose.Schema(
+  {
+    text: String,
+    author: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Msg = mongoose.model("Msg", msgSchema);
 
